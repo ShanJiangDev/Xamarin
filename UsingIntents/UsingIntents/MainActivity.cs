fd1637d542d25bf84d326355a3b31a6f299a6ff1
Shan Jiang <shanjiang.developer@gmail.com>
2016-08-24 13:58:36 +0200

    send and receive data from different activities

--- UsingIntents/UsingIntents/MainActivity.cs
+++ UsingIntents/UsingIntents/MainActivity.cs
@@ -18,7 +18,7 @@ namespace UsingIntents
 	[Activity(Label = "FirstView", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		// Checking the availability of an intent
+		// ---Checking the availability of an intent---
 		public static bool IsIntentAvailable(Context context, String action) 
 		{
 			//Context(Object):
@@ -43,6 +43,7 @@ namespace UsingIntents
 			//ResolveInfo:
 			// Information that is returned from resolving an intent against an IntentFilter. 
 			// This partially corresponds to information collected from the AndroidManifest.xml's <intent> tags.
+
 			IList<ResolveInfo> list = packageManager.QueryIntentActivities(
 				i, PackageInfoFlags.MatchDefaultOnly);
 			return list.Count > 0;
@@ -53,22 +54,22 @@ namespace UsingIntents
 		{
 			base.OnCreate(savedInstanceState);
 
-			// Set our view from the "main" layout resource
+			// ---Set our view from the "main" layout resource---
 			SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
+			// ---Get our button from the layout resource,and attach an event to it---
 			Button button = FindViewById<Button>(Resource.Id.btnSecondActivity);
 
 			button.Click += delegate 
 			{
-				// 1. Call another activity by calling the name of it
+				// 1.2.3.4: Different way to launch second activity
+				// 1. ---Call another activity by calling the name of it---
 
 				//StartActivity(typeof(SecondActivity));
 
 
 
-				// 2. Call another activity by calling "Intent"
+				// 2. ---Call another activity by calling "Intent"---
 
 				// An Intent is an object that provides runtime binding between separate components 
 				// (such as two activities). The Intent represents an app’s "intent to do something."
@@ -77,12 +78,48 @@ namespace UsingIntents
 				// Intent i = new Intent("shan.secondactivity");
 				// StartActivity(i);
 
-				// 3. Check if intent is available
+				// 3. ---Check if intent is available---
 				String action = "shan.secondactivity";
 				if (IsIntentAvailable(this, action))
 				{
+					// a.b.c: steps for sending data to another activity
+
+					// a. specify target by passing it into Intent constructor
 					Intent i = new Intent(action);
-					StartActivity(Intent.CreateChooser(i, "List of Apps"));
+
+					// b. Load intent with data, or load bundle with data and put Bundle in Intent
+					// --- Use putExtra() to add new key/value pairs.---
+					i.PutExtra("str1", "This is a string");
+					i.PutExtra("age1", 25);
+
+					// ---Use a Bundle object to add new key/value pairs.---
+					Bundle extras = new Bundle();
+					extras.PutString("str2", "This is another string");
+					extras.PutInt("age2", 19);
+					// Attach the bundle object to the Intent object
+					i.PutExtras(extras);
+
+					// c.Launch secondary activity and send the intent along with it.
+
+					// 4. ---Call another activity with extra data.---
+
+					//StartActivityForResult():
+					// A handler for when the secondary activity finishes it work
+					// and returns control to this activity.
+
+					// Starting another activity doesn't have to be one-way. You can also start
+					// another activity and receive a result back. To receive a result, call 
+					// startActivityForResult() (instead of startActivity()).
+
+					// Of course, the activity that responds must be designed to return a result.
+					// When it does, it sends the result as another Intent object. Your activity 
+					// receives it in the onActivityResult() callback.
+
+
+					StartActivityForResult( Intent.CreateChooser(i, "List of Apps"), 1);
+
+					// 3. ---Check if intent is available---
+					// StartActivity(Intent.CreateChooser(i, "List of Apps"));
 				}
 				else 
 				{
@@ -91,6 +128,23 @@ namespace UsingIntents
 				}
 
 			};
+		}
+
+		// --- Callback method for startActivityForResult() ---
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			// -- Check if the request code = 1
+			if (requestCode == 1)
+			{
+				// -- Check if the result is OK
+				if (resultCode == Result.Ok)
+				{
+					// --Get the result using getIntExtra()---
+					// --Get the result using Data()--
+					string s = data.GetIntExtra("age3", 0) + "\n" + data.Data.ToString();
+					Toast.MakeText(this, s, ToastLength.Long).Show();
+				}
+			}
 		}
 	}
 }
--- UsingIntents/UsingIntents/SecondActivity.cs
+++ UsingIntents/UsingIntents/SecondActivity.cs
@@ -39,9 +39,43 @@ namespace UsingIntents
 
 			// Create your application here
 			SetContentView(Resource.Layout.Second);
+
+			// ---Receive data from MainActivity ---
+			// a.b.c: Steps for receving data
+
+			// a: extra the data from Bundle and put it into "s"
+
+			// Get data passed in: getStringExtra() getIntExtra() from "Intent"
+			// Get the data: getString(), getInt() from "Bundle"
+
+			// "Bundle" passed by inside a "intent"
+
+			// Get Bundle object passed in
+			Bundle extras = Intent.Extras;
+			String s = Intent.GetStringExtra("str1") + "\n" +
+							 Convert.ToString(Intent.GetIntExtra("age1", 0)) + "\n" +
+							 extras.GetString("str2") + "\n" +
+							 Convert.ToString(extras.GetInt("age2"));
+
+			Toast.MakeText(this, s, ToastLength.Long).Show();
+
 			Button button = FindViewById<Button>(Resource.Id.btnClose);
 
 			button.Click += delegate {
+
+				// b: Create Intent object to return data
+				Intent i = new Intent();
+
+				// --- Use putExtra() method to retrn some value
+				i.PutExtra("age3", 99);
+
+				// c: Return value by using the setData()
+				i.SetData(Android.Net.Uri.Parse("https://www.google.se"));
+
+				// d: Set the result with OK and intent object, which will return the information back
+				// to the MainActivity
+				SetResult(Result.Ok, i);
+
 				// Distory activity
 				Finish();
 
