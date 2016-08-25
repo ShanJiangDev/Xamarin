543b266cde77ca1eb635f09667a0150be1e07d83
Shan Jiang <shanjiang.developer@gmail.com>
2016-08-25 14:37:04 +0200

    finished without testing, configuration error existed

--- LocationBasedService/LocationBasedService/MainActivity.cs
+++ LocationBasedService/LocationBasedService/MainActivity.cs
@@ -1,13 +1,20 @@
-﻿using Android.App;
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
+
+using Android.Locations;
 
 namespace LocationBasedService
 {
 	[Activity(Label = "LocationBasedService", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
+	public class MainActivity : Activity, ILocationListener
 	{
-		int count = 1;
+		LocationManager lm;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -16,12 +23,39 @@ namespace LocationBasedService
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
-
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			lm = (LocationManager)GetSystemService(Context.LocationService);
 		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			// Test on phone
+			lm.RequestLocationUpdates(LocationManager.NetworkProvider,0,0,this);
+			// Test on Emulator
+			lm.RequestLocationUpdates(LocationManager.GpsProvider, 0, 0, this);
+		}
+
+		protected override void OnPause()
+		{
+			base.OnPause();
+			lm.RemoveUpdates(this);
+		}
+
+		public void OnLocationChanged(Location loc)
+		{
+			if(loc != null) 
+			{
+				Toast.MakeText(this, "Location changed: Lat : " + loc.Latitude +
+				               " Lng: " + loc.Longitude, ToastLength.Long).Show();
+				
+			}
+		}
+
+		// Needed for ILocationListener
+		public void OnProviderDisable(string provider) { }
+		public void OnProviderEnable(string provider) { }
+		public void OnStatusChanged(string provider, Availability status, Bundle bundle) { }
+
 	}
 }
 
