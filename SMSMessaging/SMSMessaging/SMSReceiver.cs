2d2a450be60f40f59392959b27180a3adda69b94
Shan Jiang <shanjiang.developer@gmail.com>
2016-08-25 11:34:26 +0200

    SMSReceiver finished without testing

--- SMSMessaging/SMSMessaging/SMSReceiver.cs
+++ SMSMessaging/SMSMessaging/SMSReceiver.cs
@@ -11,14 +11,48 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+using Android.Telephony;
+
 namespace SMSMessaging
 {
 	[BroadcastReceiver]
+	[IntentFilter (new[] { "android.provider.Telephone.SMS_RECEIVED"}, Priority = 1000)]
+
 	public class SMSReceiver : BroadcastReceiver
 	{
 		public override void OnReceive(Context context, Intent intent)
 		{
-			Toast.MakeText(context, "Received intent!", ToastLength.Short).Show();
+			Bundle bundle = intent.Extras;
+			SmsMessage[] msgs = null;
+			String msg = "";
+			String tel = "";
+			if (bundle != null) 
+			{
+				// Retrieve the sms message received
+				Java.Lang.Object[] pdus = (Java.Lang.Object[])bundle.Get("pdus");
+				msgs = new SmsMessage[pdus.Length];
+
+				for (int i = 0; i < msgs.Length; i++) 
+				{
+					msgs[i] = SmsMessage.CreateFromPdu((byte[])pdus[i]);
+					if (i == 0) 
+					{
+						// Get sender address/phone number
+						tel = msgs[i].OriginatingAddress;
+					}
+					// Get message body
+					msg += msgs[i].MessageBody;
+				}
+
+				// Display the sms message
+				Toast.MakeText(context, tel + ": " + msg, ToastLength.Long).Show();
+				if (msg.StartsWith("intercept"))
+				{
+					InvokeAbortBroadcast();
+				}
+				
+			}
+
 		}
 	}
 }
